5d2d928ad475718b71ddef166e293a552a76314f
Vikgthub <164849216+Vikgthub@users.noreply.github.com>
2026-01-06 14:10:07 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+
+
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, '/')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Amir Assessment 2 Web Dev .html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
